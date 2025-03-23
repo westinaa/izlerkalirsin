04ece51e859d36515c6553b651a69958cb6eb9cc
westinaa <162748407+westinaa@users.noreply.github.com>
2025-03-24 01:49:40 +0300

    Update streamerUserChannel.js

--- settings/schemas/streamerUserChannel.js
+++ settings/schemas/streamerUserChannel.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 
 const streamerUserChannelSchema = new mongoose.Schema({
-    guildID: { type: String, required: true },
-    userID: { type: String, required: true },
-    ChannelID: { type: String, required: true }, // Buraya ekliyoruz
-    ChannelData: { type: Number, default: 0 }
+  guildID: { type: String, required: true },
+  userID: { type: String, required: true },
+  channelID: { type: String, required: true },  // channelID burada tanımlanmalı
+  channelData: { type: Number, default: 0 },
+  // Diğer alanlar burada
 });
 
-const StreamerUserChannel = mongoose.model('StreamerUserChannel', streamerUserChannelSchema);
-
-module.exports = StreamerUserChannel;
+const streamerUserChannel = mongoose.model('StreamerUserChannel', streamerUserChannelSchema);
