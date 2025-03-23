f554ad83b742e62d7dd23a35ffcb46d7c9ecdb31
westinaa <162748407+westinaa@users.noreply.github.com>
2025-03-24 01:31:02 +0300

    Update voiceUser.js

--- settings/schemas/voiceUser.js
+++ settings/schemas/voiceUser.js
@@ -3,6 +3,7 @@ const { Schema, model } = require("mongoose");
 const schema = Schema({
   guildID: String,
   userID: String,
+  channelID: String,
   topStat: { type: Number, default: 0 },
   dailyStat: { type: Number, default: 0 },
   weeklyStat: { type: Number, default: 0 },
