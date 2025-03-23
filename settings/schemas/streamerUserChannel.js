becb9be72efbb67f923ce9b576ea6eb41359bd4e
westinaa <162748407+westinaa@users.noreply.github.com>
2025-03-23 23:58:03 +0300

    Update streamerUserChannel.js

--- settings/schemas/streamerUserChannel.js
+++ settings/schemas/streamerUserChannel.js
@@ -3,8 +3,8 @@ const { Schema, model } = require("mongoose");
 const StreamerUserChannel = Schema({
     guildID: String,
     userID: String,
-    ChannelID: String,
-    ChannelData: Number,
+    channelID: String,
+    channelData: Number,
 });
 
 module.exports = model("StreamerUserChannel", StreamerUserChannel);
