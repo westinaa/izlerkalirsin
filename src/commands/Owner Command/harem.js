dc89de726f49731ba54f29359251d40f22ba9de8
westinaa <162748407+westinaa@users.noreply.github.com>
2025-04-14 00:21:17 +0300

    Update harem.js

--- src/commands/Owner Command/harem.js
+++ src/commands/Owner Command/harem.js
@@ -13,25 +13,24 @@ module.exports = {
   run: async (client, message, args, embed, prefix) => {
     const allowedUsers = ["812204329535668234", "474006896408264712"];
     const roleId = "1357152571776827573";
-    
-    let kanallar = ayar.KomutKullanımKanalİsim;
-    if (!allowedUsers.includes(message.author.id) && !kanallar.includes(message.channel.name)) {
-      return message.reply({ content: `${kanallar.map(x => `${client.channels.cache.find(chan => chan.name == x)}`)} kanallarında kullanabilirsiniz.`})
+
+    if (!allowedUsers.includes(message.author.id)) {
+      return message.reply("Harem kontrolü yalnızca sultanıma aittir!")
         .then((e) => setTimeout(() => { e.delete(); }, 10000));
     }
-    
+
     if (!message.guild) return;
-    
+
     const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
     if (!member) {
       return message.reply("<a:amw:1353018181886677094> **Sultanım**, \nLütfen haremine ekleyeceğin kullanıcıyı etiketle...");
     }
-    
+
     const role = message.guild.roles.cache.get(roleId);
     if (!role) {
       return message.reply("Belirtilen rol bulunamadı.");
     }
-    
+
     if (member.roles.cache.has(roleId)) {
       await member.roles.remove(roleId);
       return message.reply(`<a:abowblue2:1353018169551360011> **Sultanım Çok Yaşa!** \n${member} haremden kovuldu!`);
