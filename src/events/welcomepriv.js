8f420507febfafd962683371ae39cdec26efe4c2
westinaa <w3stiin4@gmail.com>
2025-04-15 00:55:48 +0300

    wpr

--- src/events/welcomepriv.js
+++ src/events/welcomepriv.js
@@ -21,15 +21,15 @@ let row = new ActionRowBuilder().addComponents(
     buton1
 )
 
-chatchannel.send({content: `__Hoşgeldin Aramıza Biri Katıldı__ <@${member.id}> __Ailemize Hoşgeldin Dostum__`, components: [row]}).then((e) => setTimeout(() => { e.delete(); }, 5000));
+chatchannel.send({content: `**<@${member.id}> ARAMIZA KATILDI, SELAM VERMEYİ UNUTMAYIN!**`, components: [row]}); //.then((e) => setTimeout(() => { e.delete(); }, 5000));
 const usedButtons = new Set();
 const collector = chatchannel.createMessageComponentCollector({})
 collector.on('collect', async (interaction) => {
     if(interaction.customId === "selam"){
       if(usedButtons.has(interaction.user.id)){
-        interaction.reply({content: `Sanırım selam vermeyi 5 saniye önce kullandın tekrar dene.`, ephemeral: true});//.then((e) => setTimeout(() => { e.delete(); }, 5000));
+        interaction.reply({content: `Selam vermek için 5 saniye beklemelisin.`, ephemeral: true}).then((e) => setTimeout(() => { e.delete(); }, 5000));
       } else {
-        interaction.reply(`**<@${uye.id}>, <@${interaction.user.id}> Sana Selam Verdi!**`).then((e) => setTimeout(() => { e.delete(); }, 5000));
+        interaction.reply(`**<@${uye.id}>, <@${interaction.user.id}> sana selam verdi!**`).then((e) => setTimeout(() => { e.delete(); }, 5000));
         usedButtons.add(interaction.user.id);
         setTimeout(() => {
           usedButtons.delete(interaction.user.id);
